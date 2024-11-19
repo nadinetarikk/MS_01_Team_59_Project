65a9180a4e97b442af5bee4ea5d305edf40a737b
Nadine <nadinetariknour@hotmail.com>
2024-11-19 20:16:12 +0200

    MS 2 temp code

---
+++ MS_02.c
@@ -0,0 +1,33 @@
+#include "pico/stdlib.h"
+#include "infrared_sensor.h"
+
+// Define the default pin for the infrared sensor
+#ifndef IR_SENSOR_PIN
+// Default GPIO pin for infrared sensor
+#define IR_SENSOR_PIN 15
+#endif
+
+int main()
+{
+    stdio_init_all();
+
+    // Initialize the infrared sensor on GPIO 15
+    infrared_sensor_init(IR_SENSOR_PIN);
+    {
+        while (true)
+        {
+            if (infrared_sensor_is_triggered(IR_SENSOR_PIN))
+            {
+                // Do something when the sensor is triggered
+                printf("Object detected!\n");
+            }
+            else
+            {
+                // Do something else when the sensor is not triggered
+                printf("No object detected.\n");
+            }
+            sleep_ms(100); // Wait for a short delay for debouncing
+        }
+        return 0;
+    }
+}
---
+++ infrared_sensor/infrared_sensor.h
@@ -0,0 +1,9 @@
+#ifndef INFRARED_SENSOR_H
+#define INFRARED_SENSOR_H
+#include "pico/stdlib.h"
+// Function to initialize the infrared sensor
+void infrared_sensor_init (uint sensor_pin);
+// Function to read the state of the infrared sensor
+bool infrared_sensor_is_triggered (uint sensor_pin);
+#endif
+// INFRARED_SENSOR_H
